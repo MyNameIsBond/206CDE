436e71ab24fe783d101d5d92cfa7fb484afdb66f
Tony Hajdini <hajdinim@uni.coventry.com>
2018-02-24 17:24:32 +0000

    JS g.

--- static/js/hml.js
+++ static/js/hml.js
@@ -1,6 +1,9 @@
-console.log('test')
 function test() {
     document.getElementById('test').addEventListener('click',() => {
         console.log('test')
     })
+}
+
+function test1() {
+    console.log('test')
 }
