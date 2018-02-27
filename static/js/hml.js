59682262db32cce0e30d2128d619871cc0cb0950
Mition Hajdini <hajdinim@uni.coventry.ac.uk>
2018-02-27 09:44:44 +0000

    cleaner code

--- static/js/hml.js
+++ static/js/hml.js
@@ -1,3 +1,30 @@
-function change_function(x) {
-    console.log(x.value);
+
+document.getElementById('test').addEventListener('click',() => {
+    console.log('test')
+})
+
+
+
+function change_function(){
+    let valuee = document.getElementById('opt').value
+    console.log(valuee)
+}
+
+
+document.getElementById('opt').addEventListener('onchange', () => {
+    console.log(value);
+})
+
+function functionName() {
+
+}
+
+document.getElementById('hml').addEventListener('click', () => {
+    hml = new XMLHttpRequest()
+
+})
+
+function change_function(x){
+    console.log(x.value)
+    console.log(x)
 }
