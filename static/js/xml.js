d9abcbaec5bb604c1ade48470d5142a1367a0e42
Mition Hajdini <hajdinim@uni.coventry.ac.uk>
2018-03-05 16:23:25 +0000

    cleaner code.

--- static/js/xml.js
+++ static/js/xml.js
@@ -1,18 +1,13 @@
-
-
 // recieves the Currencies from back end. 
 function send_data(){
     const cc_value = document.getElementById('cc_value').value
     const c_value = document.getElementById('c_value').value
 
-    console.log(cc_value,c_value)
     const xml = new XMLHttpRequest()
 
     xml.onload = function() {
         if (this.status === 200 && this.readyState === 4){
             document.getElementById('c_response').innerHTML = this.responseText
-            console.log(this.responseText)
-            console.log('hey')
         } else {
             const error = 'something went wrong'
             console.log(error,this.statusText)
